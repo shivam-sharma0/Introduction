3d858b14aa9e7203d84dbda404158401f9638864
shivamvashist001 <124781117+shivamvashist001@users.noreply.github.com>
2023-03-17 11:39:15 +0530

    Add files via upload

---
+++ Intro/LearnSwing.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LearnSwing {
+  public static void main(String[] args) {
+     JFrame frame=new JFrame();
+     frame.setTitle("Introdution");
+     frame.setLocation(200,200);
+     frame.setSize(800,600);
+     frame.getContentPane().setBackground(Color.cyan);
+     frame.setVisible(true);
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
